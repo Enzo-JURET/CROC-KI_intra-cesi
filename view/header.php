f5715e15d8ab39a6a72ff2e16680af2cccea336c
LehouxKevin <kevin.lehoux5@hotmail.fr>
2020-02-26 14:05:20 +0100

    Ajout de bootstrap et Jquery.  Début d'entête

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+
+        <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="public/css/header.css" type="text/css" />
+    </head>
+    <body>
+        ceci est un entête
+    </body>
+</html>
