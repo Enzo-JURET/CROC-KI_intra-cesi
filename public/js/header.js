4ce44a3ecbcdafb2c9bfeabf6057a0eb5493adff
Enzo-JURET <juretenzo@gmail.com>
2020-02-27 17:03:24 +0100

    Ajout animation de la navbar

--- public/js/header.js
+++ public/js/header.js
@@ -1,14 +1,23 @@
 $( document ).ready(function() {
+
 	console.log("debut script");
-	var x = document.getElementById("iconeCentral");
-	x.mouseover(function() {
-    	console.log("debut de la fonction mouseover");
-    	$(".iconeCentral").hide();
-    	$(".NavBar").show();
-    })
-    /**$(".iconeCentral").mouseover(function() {
-    	console.log("debut de la fonction mouseover");
-    	$(".iconeCentral").hide();
-    	$(".NavBar").show();
-    })**/
-});
+	//$("#iconeCentral").mousemove(function() {
+    $('#iconeCentral').mouseenter(function() {
+        // le curseur passe sur l'élément
+        console.log("checkpoint 1");
+        $("#iconeCentral").css("visibility","hidden")
+        $("#NavBar").css("visibility","visible");
+        $("#NavBar").slideDown("slow").delay(800).fadeIn(400);
+    }), $('#NavBar').mouseleave(function() {
+        console.log("checkpoint 1");
+        $("#NavBar").css("visibility","hidden")
+        $("#iconeCentral").css("visibility","visible");    
+    });
+    /*,function() {
+    // le curseur sort de l'élément
+    console.log("checkpoint 1");
+    $("#NavBar").css("visibility","hidden")
+    $("#iconeCentral").css("visibility","visible");*/
+
+    });
+//});
