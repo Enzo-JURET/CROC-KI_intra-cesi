61a8d2f8d37355d17d3c31856c1cd2b8d4242be8
Enzo-JURET <juretenzo@gmail.com>
2020-02-29 00:26:35 +0100

    Modification temporaire de la NavBar, Modification de la page profil -> Ajout de plusieurs features

--- view/header.php
+++ view/header.php
@@ -1,4 +1,5 @@
 <img id="iconeCentral" class="iconeCentral" src="../public/images/icones/menu.png">
 <nav id="NavBar" class="col-sm-12 navbar NavBar">
-    <img id="iconeProfil" class="iconeBarre" href="../view/index.php" src="../public/images/icones/profil.png" alt="Profil">
+    <a href="../view/profil.php"><img class="iconeBarre" href="../view/index.php" src="../public/images/icones/profil.png" alt="Profil"></a>
+    <a href="../view/conversation.php"><img class="iconeBarre" href="../view/index.php" src="../public/images/icones/groupes.png" alt="Profil"></a>
 </nav>
--- view/profil.php
+++ view/profil.php
@@ -16,8 +16,97 @@
 			<img id="image-banniere" class="img-fluid" src="../public/images/banniere-test.jpg">
 			<img id="image-profil" src="../public/images/avatar-test.jpg"><
 			<div class="row">
-				<div id="pseudo-info" class="col-sm-2 offset-sm-4">
-					Zazo72
+				<div class="col-sm-3 offset-sm-3">
+					<p id="pseudo-info">
+						Enzo JURET
+					</p>
+					<p id="promo-info">	
+						<a href="../view/index.php">RIL-PROMO-201</a>
+					</p>
+				</div>
+				<div class="image-envoi-message col-sm-1 offset-sm-1 boutons-info-profil-demander-en"></div>
+				<div class="image-ajouter-ami col-sm-1 offset-sm-1 boutons-info-profil-demander-en"></div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4 offset-sm-1">
+					<p id="titre-description">
+						Description :
+					</p>
+					<hr id="ligne-description">
+					<p id="description-info">
+						Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié.
+					</p>
+				</div>
+				<div class="col-sm-3">
+					<div id="bandeau-info">
+						Liste des groupes
+					</div>
+					<div class="liste-info">
+						<p>
+							<a href="../view/profil.php">RIL-PROMO-201</a><br>
+							CROC-KI<br>
+							KIKENDIM<br>
+							<a href="../view/profil.php">Les 24H du code</a><br>
+						</p>
+					</div>
+				</div>
+				<div class="col-sm-3">
+					<div id="bandeau-info">
+						Liste d'amis
+					</div>
+					<div class="liste-info">
+						<p>
+							<a href="../view/profil.php">Kévin LEHOUX</a><br>
+							<a href="../view/profil.php">Justin BAHIER</a><br>
+							<a href="../view/profil.php">Kilian POITOU</a><br>
+							<a href="../view/profil.php">Dimitri LEVILLAIN</a><br>
+							<a href="../view/profil.php">Allan LENOGUE</a><br>
+							<a href="../view/profil.php">Sam ROY</a><br>
+							<a href="../view/profil.php">Bryan DELMAS</a><br>
+							<a href="../view/profil.php">Dimitri CIRON</a><br>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div id="info-contact" class="row">
+				<div class="col-sm-3 offset-sm-1">
+					<p id="titre-info-contact-profil">
+						Informations supplémentaires :
+					</p>
+					<hr id="ligne-info-contact">
+					<p id="info-contact-profil">
+						<a href="../view/profil.php">enzo.juret@viacesi.fr</a>
+					</p>
+					<p id="info-contact-profil">
+						<a href="../view/profil.php">0663865914</a>
+					</p>
+				</div>
+				<div class="col-sm-6 offset-sm-1">
+					<div id="bandeau-info">
+						Compétences de l'élève
+					</div>
+					<div class="liste-info">
+						<p>
+							<label class="barre-progress-info" for="file">PHP</label>
+							<progress id="file" max="100" value="20"> 20% </progress>
+						</p>
+						<p>
+							<label class="barre-progress-info" for="file">Python</label>
+							<progress id="file" max="100" value="60"> 20% </progress>
+						</p>
+						<p>
+							<label class="barre-progress-info" for="file">Java</label>
+							<progress id="file" max="100" value="50"> 20% </progress>
+						</p>
+						<p>
+							<label class="barre-progress-info" for="file">HTML</label>
+							<progress id="file" max="100" value="80"> 20% </progress>
+						</p>
+						<p>
+							<label class="barre-progress-info" for="file">CSS (feuille de style)</label>
+							<progress id="file" max="100" value="70"> 20% </progress>
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
