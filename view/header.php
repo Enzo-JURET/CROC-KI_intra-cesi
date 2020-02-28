01f36b7525977822792c114526a1d353da5a7791
Enzo-JURET <juretenzo@gmail.com>
2020-02-28 16:01:23 +0100

    Création de la page profil ainsi que sont css, modification temporaire de la navbar

--- view/header.php
+++ view/header.php
@@ -1,6 +1,4 @@
 <img id="iconeCentral" class="iconeCentral" src="../public/images/icones/menu.png">
-<nav id="NavBar" class="navbar NavBar">
-    <a href="../view/index.php"><img id="iconeProfil" class="iconeBarre" href="../view/index.php" src="../public/images/icones/profil.png" alt="Profil"></a>
-    <a href="../view/index.php"><img id="iconeGroupe" class="iconeBarre" src="../public/images/icones/groupes.png" alt="Groupe"></a>
-    <a href="../view/index.php"><img id="iconeActualite" class="iconeBarre" src="../public/images/icones/actualites.png" alt="Actualites"></a>
+<nav id="NavBar" class="col-sm-12 navbar NavBar">
+    <img id="iconeProfil" class="iconeBarre" href="../view/index.php" src="../public/images/icones/profil.png" alt="Profil">
 </nav>
---
+++ view/profil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+	<link rel="stylesheet" href="../public/css/bootstrap/bootstrap.min.css" type="text/css"/>
+    <link rel="stylesheet" href="../public/css/header.css" type="text/css"/>
+    <link rel="stylesheet" href="../public/css/profil.css" type="text/css"/>
+    <link rel="stylesheet" href="../public/css/style.css" type="text/css"/>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="../public/js/header.js"></script>
+</head>
+<body>
+	<?php require('../view/header.php'); ?>
+	<div class="row page-profil">
+		<div id="block-profil" class="col-sm-10 offset-sm-1">
+			<img id="image-banniere" class="img-fluid" src="../public/images/banniere-test.jpg">
+			<img id="image-profil" src="../public/images/avatar-test.jpg"><
+			<div class="row">
+				<div id="pseudo-info" class="col-sm-2 offset-sm-4">
+					Zazo72
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
