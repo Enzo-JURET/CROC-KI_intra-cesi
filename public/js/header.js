d5fa9d3dda0452fbde355fe89c0eb9b4839812a5
Enzo-JURET <juretenzo@gmail.com>
2020-02-28 09:36:13 +0100

    Modification de la NavBar | Ajout des icones groupe et actualite

--- public/js/header.js
+++ public/js/header.js
@@ -1,9 +1,6 @@
 $( document ).ready(function() {
-
 	console.log("debut script");
-	//$("#iconeCentral").mousemove(function() {
     $('#iconeCentral').mouseenter(function() {
-        // le curseur passe sur l'élément
         console.log("checkpoint 1");
         $("#iconeCentral").css("visibility","hidden")
         $("#NavBar").css("visibility","visible");
@@ -13,11 +10,4 @@ $( document ).ready(function() {
         $("#NavBar").css("visibility","hidden")
         $("#iconeCentral").css("visibility","visible");    
     });
-    /*,function() {
-    // le curseur sort de l'élément
-    console.log("checkpoint 1");
-    $("#NavBar").css("visibility","hidden")
-    $("#iconeCentral").css("visibility","visible");*/
-
-    });
-//});
+});
