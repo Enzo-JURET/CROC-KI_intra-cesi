50922c08a34134d03d4341dffe6f04582ced2869
LehouxKevin <kevin.lehoux5@hotmail.fr>
2020-02-28 15:53:11 +0100

    conversation avancement, fond et thème

--- view/conversation.php
+++ view/conversation.php
@@ -7,14 +7,55 @@
         <link rel="stylesheet" href="../public/css/bootstrap/bootstrap.min.css" type="text/css" />
         <link rel="stylesheet" href="../public/css/header.css" type="text/css" />
         <link rel="stylesheet" href="../public/css/conversation.css" type="text/css" />
+
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="../public/js/conversation.js"></script>
     </head>
     <body>
-        <?php require('header.php'); ?>
+    <?php require('header.php'); ?>
 
-        <div id="bandeauMessage">Conversation avec Enzo Juret</div>
-        <div id="menuCentral" class="container">
-            oui
+    <div id="boiteFenetre" class="col-sm-12">
+        <div id="boiteContact" class="col-sm-3 boitesPrincipales">
+            <div id="bandeauContact" class="bandeauTitre">Contacts</div>
+            <div id="conteneurContact" class="conteneur">
+                
+            </div>
         </div>
+
+        <div id ="boiteMessagerie" class="col-sm-6 boitesPrincipales">
+            <div id="bandeauMessage" class="bandeauTitre">Conversation avec Enzo Juret</div>
+            <div id="conteneurMessage" class="conteneur">
+                <div id="corpsConteneurMessage">
+
+                </div>
+                <div id="footerConteneurMessage">
+                    coucou
+                </div>
+            </div>
+        </div>
+
+        <div id ="boiteGroupe" class="col-sm-3 boitesPrincipales">
+            <div id="bandeauGroupe" class="bandeauTitre">Groupes</div>
+            <div id="conteneurGroupe" class="conteneur">
+                <div id="divGroupePrive">
+                    <div id="bandeauGroupePrive" class="bandeauTypeGroupe">
+                        <div class="titreTypeGroupe">Groupes privés</div>
+                    </div>
+                    <div id="conteneurGroupePrive" class="conteneurTypeGroupe">
+                        <p class="ligneGroupe">RIL 2019</p>
+                    </div>
+                </div>
+                <div id="divGroupePublic">
+                    <div id="bandeauGroupePublic" class="bandeauTypeGroupe">
+                        <b class="titreTypeGroupe">Groupes publics</b>
+                    </div>
+                    <div id="conteneurGroupePublic" class="conteneurTypeGroupe">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
         
     </body>
 </html>
