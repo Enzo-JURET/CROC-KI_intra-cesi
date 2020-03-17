364fb3815d561e3d192590d96b755593662b2de2
Enzo-JURET <juretenzo@gmail.com>
2020-03-17 18:24:48 +0100

    ...

--- controller/controller.php
+++ controller/controller.php
@@ -13,6 +13,12 @@
                         $result = $this->getFriends();
                     }
                 break;
+                case "authentification":
+                    if($_SERVER['REQUEST_METHOD'] === 'POST')
+                    {
+                        $result = $this->Authentification();
+                    }
+                break;
             }
             return $result;
         }
@@ -27,6 +33,38 @@
             $personne = $dbcontroller->executeSelectQuery($stmt);
             $dbcontroller->closeQuery();
             return json_encode($personne);
+        }
+
+        function Authentification()
+        {
+            $retour=[];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $dbcontroller = new dbController();
+
+            $stmt = mysqli_prepare($dbcontroller->getConn(),
+                "SELECT id_personne, e_mail_personne, password_personne, nom_personne, prenom_personne, id_role, id_promotion FROM personne WHERE e_mail_personne = ?");
+            mysqli_stmt_bind_param($stmt,'s',$email);
+            $resultat = $dbcontroller->executeSelectQuery($stmt);
+            $dbcontroller->closeQuery();
+            
+            if (($resultat[0]["e_mail_personne"] == $email) && ($resultat[0]["password_personne"] == $password)){
+                
+                $retour["id"] = $resultat[0]["id_personne"];    
+                $retour["email"] = $email;
+                $retour["password"] = $password;
+                $retour["nom"] = $resultat[0]["nom_personne"];
+                $retour["prenom"] = $resultat[0]["prenom_personne"];
+                $retour["role"] = $resultat[0]["id_role"];
+                $retour["promotion"] = $resultat[0]["id_promotion"];
+                $retour["etat"] = true;
+
+            }
+            else {
+                $retour["etat"] = false;
+            }
+
+            return json_encode($retour);
         }
     }
 ?>
--- view/connexion.php
+++ view/connexion.php
@@ -8,8 +8,10 @@
     <link rel="stylesheet" href="../public/css/style.css" type="text/css"/>
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="../public/js/header.js"></script>
+    <script type="text/javascript" src="../model/connexion.js"></script>
 </head>
 <body class="background">
+	<form id="form-connexion">
 	<div class="row page-profil">
 		<div id="block-profil" class="col-sm-4 offset-sm-4">
 			<h2 class="title">
@@ -19,20 +21,21 @@
 		<div class="row page-profil">
 			<div class="col-sm-10 offset-sm-1">
 				<p>
-				 	<input class="champ" type="text" placeholder="Entrer le nom d'utilisateur" name="login" required>
+					<input id="email" class="champ" type="text" placeholder="Entrer votre Email" name="email" required>
 				</p>
 				<p>
-					<input class="champ" type="password" placeholder="Entrer le mot de passe" name="password" required>
+					<input id="password" class="champ" type="password" placeholder="Entrer le mot de passe" name="password" required>
 				</p>
 			</div>
 		</div>
 		<div class="row page-profil">
 			<div class="col-sm-8 offset-sm-2">
 				<p>
-					<input type="submit" class="bouton-login" value='LOGIN' >
+					<input type="submit" id="valider_connexion" class="bouton-login" value='LOGIN'>
 				</p>
 			</div>
 		</div>
 	</div>
+	</form>
 </body>
 </html>
